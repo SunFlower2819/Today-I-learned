00b103dcfd0f173fba5b4d7397681554e1a94942
Sun Taeuk <130738283+SunFlower2819@users.noreply.github.com>
2023-08-13 11:02:30 +0900

    푸는중

---
+++ Algorithm/알고리즘 문제/최소힙.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// 최소 힙. --> 배열로 구현
+// 0번 인덱스는 비우기
+
+// 구조체 (우선순위, 데이터)
+// 구조체 (힙, 현재 저장된 데이터 수)
+
+class HeapData
+{
+	int data;
+	int priority;
+};
+
+class Heap
+{
+	HeapData* heap;
+	int numOfData;
+};
+
+// 필요한 연산들.
+// 1. 힙 생성 연산
+// 2. IsEmpty()
+// 3. IsFull()
+// 4. 데이터 추가 연산
+// 5. 데이터 삭제 연산
+
+int main()
+{
+
+	return 0;
+}
