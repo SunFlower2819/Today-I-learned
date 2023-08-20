575693596f1dc9a30639e41c63c9a71d4f3c43ba
Sun Taeuk <130738283+SunFlower2819@users.noreply.github.com>
2023-08-20 21:21:04 +0900

    Update 최소힙.cpp

--- Algorithm/알고리즘 문제/최소힙.cpp
+++ Algorithm/알고리즘 문제/최소힙.cpp
@@ -18,7 +18,7 @@ public:
 class Heap
 {
 public:
-	HeapData hBt[dataSize] = {0};
+	HeapData hBt[dataSize] = { 0 };
 	int numOfData;
 
 	Heap()
@@ -28,13 +28,11 @@ public:
 };
 
 // 필요한 연산들.
-// 1. 힙 생성 연산
-// 2. IsEmpty()
-// 3. IsFull()
-// 4. 데이터 추가 연산
-// 5. 데이터 삭제 연산
+// 1. IsEmpty()
+// 2. IsFull()
+// 3. 데이터 추가 연산
+// 4. 데이터 삭제 연산
 
-void InitHeap(Heap* heap);
 bool IsEmpty(Heap* heap);
 bool IsFull(Heap* heap);
 void AddData(Heap* heap, int data);
@@ -44,12 +42,37 @@ int main()
 {
 	Heap heap;
 
-	return 0;
-}
+	//int N;
+	//std::cin >> N;
 
-void InitHeap(Heap* heap)
-{
-	
+	//for (int i = 0; i < N; i++)
+	//{
+	//	int x;
+	//	std::cin >> x;
+
+	//	if (x == 0)
+	//	{
+	//		if (IsEmpty(&heap))
+	//			std::cout << "데이터가 없습니다." << std::endl;
+	//		else
+	//			DeleteData(&heap);
+	//	}
+	//	else
+	//	{
+	//		if (IsFull(&heap))
+	//			std::cout << "더 이상 데이터를 추가할 수 없습니다." << std::endl;
+	//		else
+	//			AddData(&heap, x);
+	//	}
+	//}
+
+	AddData(&heap, 4);
+	AddData(&heap, 2);
+	AddData(&heap, 5);
+	AddData(&heap, 1);
+	AddData(&heap, 3);
+
+	return 0;
 }
 
 bool IsEmpty(Heap* heap)
@@ -71,16 +94,59 @@ bool IsFull(Heap* heap)
 // 힙 삽입연산 구현해야함.
 void AddData(Heap* heap, int num)
 {
+	// 1. 마지막 위치에 노드 삽입
+	// 2. 루트 노드와 비교
+	// 3. 루트 노드보다 작을 시 값 교환
+	// 4. 루트 노드보다 클 시 삽입 완료
+
 	heap->numOfData += 1;
-	int lastIndex = heap->numOfData;
+	int index = heap->numOfData;
 
-	heap->hBt[lastIndex].data = num;
+	// 마지막 위치에 노드 저장
+	heap->hBt[index].data = num;
+	heap->hBt[index].priority = index;
 
+	// 삽입 노드의 인덱스가 1이면 중지
+	while (index != 1)
+	{
+		// 삽입 노드와 루트 노드 비교 후 삽입 노드가 작으면 값 교환
+		// 아니면 삽입 완료
+		if (heap->hBt[index].data < heap->hBt[index / 2].data)
+		{
+			int temp = heap->hBt[index].data;
+			heap->hBt[index].data = heap->hBt[index / 2].data;
+			heap->hBt[index / 2].data = temp;
+
+			index /= 2;
+		}
+		else
+			break;
+	}
+
+	std::cout << heap->hBt[index].data << "삽입 완료" << std::endl;
 }
 
 int DeleteData(Heap* heap)
 {
-	int deleteData = 0;
+	// 1.마지막 노드를 첫 번째 노드 위치로 옮기기
+	// 2.그리고 왼쪽 노드와 오른쪽 노드 중 더 작은 노드와 교환 (물론 선택된 노드보다 작아야함)
+	// 3.삭제 완료
+
+	int deleteData = heap->hBt[1].data;
+	int index = heap->numOfData;
+
+	// 마지막 노드의 데이터를 루트 노드에 넣어준다. 그리고 마지막 노드의 데이터를 0으로 초기화
+	heap->hBt[1].data = heap->hBt[index].data;
+	heap->hBt[index].data = 0;
+	
+
+
+	// 여기까지 루트 노드에 마지막 노드를 삽입해주는 작업을 맞췄어.
+	// 아래부터는 자식 노드와 비교 후 더 작은 값이랑 값을 교환해주는 작업을 해야 돼.
+
+	// 내 생각엔 왼쪽 노드와 오른쪽 노드와 비교후
+	// 더 작은 값이랑 루트 노드랑 비교 후 
+	// 자식 노드가 더 작을 시 교환해주는 메커니즘을 사용해야 할 거 같아.
 
 	return deleteData;
 }
