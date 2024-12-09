dffd715349988b07f76ba273834ebaab74ad4ee6
Sun Taeuk <130738283+SunFlower2819@users.noreply.github.com>
2024-12-09 16:21:45 +0900

    백준 3052번 : 이게 최선일까...?

--- Algorithm/알고리즘 문제/3052번: 나머지.cpp
+++ Algorithm/알고리즘 문제/3052번: 나머지.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-int main() 
+int main()
 {
 	// vector에 값을 입력, 그 값을 42로 나눈 값을 벡터에 저장
 	std::vector<int> v(10);
@@ -13,7 +13,7 @@ int main()
 	}
 
 	// 정렬
-	sort(v.begin(), v.end());
+	std::sort(v.begin(), v.end());
 
 	// compare은 정렬된 원소를 순차적으로 비교하다가 달라지는 지점을 나타내는 변수
 	// count는 달라지는 지점을 세는 변수
